409e78675bd090d08ba99b6b1de7bb88daa9010b
suzu <suzuXIV@gmail.com>
2023-10-14 19:40:44 +0800

    fix error

--- lua/core/color-scheme.lua
+++ lua/core/color-scheme.lua
@@ -17,7 +17,7 @@ require("kanagawa").setup({
 		}
 	end,
 })
-vim.cmd("colorscheme kanagawa-dragoon")
+vim.cmd("colorscheme kanagawa-dragon")
 local colors = require("kanagawa.colors").setup({ })
 local theme = colors.theme
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = theme.ui.bg_p1 })
