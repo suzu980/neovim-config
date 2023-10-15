d73664b1991b6c38b1cfaed8a781a4a802a1e1bf
suzu <suzuXIV@gmail.com>
2023-10-15 19:42:41 +0800

    Cleaner dashboard

--- lua/core/alpha-config.lua
+++ lua/core/alpha-config.lua
@@ -23,12 +23,11 @@ dashboard.section.header.val = {
 }
 
 dashboard.section.buttons.val = {
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", "  Open Configuration File", ":e ~/.config/nvim/init.lua<CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+	dashboard.button("e", "🗎   New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("r", "🗁   Recent files", ":Telescope oldfiles <CR>"),
+	dashboard.button("f", "🔍  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("t", "🗛   Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("q", "⍇   Quit", ":qa<CR>"),
 }
 
 local function footer()
@@ -37,8 +36,8 @@ end
 
 dashboard.section.footer.val = footer()
 
-dashboard.section.footer.opts.hl = "Type"
-dashboard.section.header.opts.hl = "Include"
+dashboard.section.footer.opts.hl = "Include"
+dashboard.section.header.opts.hl = "Identifier"
 dashboard.section.buttons.opts.hl = "Keyword"
 
 dashboard.opts.opts.noautocmd = true
